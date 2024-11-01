924cf810d2d3eb3155a38a4e91d4bdf24b9b7b49
faezsal <faez_sal@yahoo.com>
2024-11-01 03:07:45 +0100

    Update Api.php

--- app/tests/Api/Api.php
+++ app/tests/Api/Api.php
@@ -31,7 +31,7 @@ class Api extends ApiTestCase
 
             UserFactory::createOne(["email" => "admin@admin.com", "password" => "1Qq!1111"]);
             $user = UserFactory::findBy(["email" => "admin@admin.com"]);
-            var_dump($user[0]);
+            var_dump($user[0]->getEmail());
 
             $response = static::createClient()->request('POST', '/api/login_check', [
             'json' => [
